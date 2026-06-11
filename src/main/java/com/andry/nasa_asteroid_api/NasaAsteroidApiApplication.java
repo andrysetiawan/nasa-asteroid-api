4592512e3ce384b075409cd8fadc7d8ba8caf9cd
Andry Setiawan <yustinusandrys@gmail.com>
2026-06-12 01:19:14 +0700

    project initialization

---
+++ src/main/java/com/andry/nasa_asteroid_api/NasaAsteroidApiApplication.java
@@ -0,0 +1,13 @@
+package com.andry.nasa_asteroid_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaAsteroidApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaAsteroidApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andry/nasa_asteroid_api/NasaAsteroidApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andry.nasa_asteroid_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaAsteroidApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
